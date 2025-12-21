d69dd227e033e635c0618f445770f79cd42abda7
Alexandr Faleev <aaffaleev@gmail.com>
2025-12-21 14:15:57 +0300

    feat: add footer component

--- src/components/layout/PageWrapper/PageWrapper.tsx
+++ src/components/layout/PageWrapper/PageWrapper.tsx
@@ -3,13 +3,14 @@ import clsx from 'clsx';
 
 import s from './PageWrapper.module.scss';
 import { Header } from '../Header';
+import { Footer } from '../Footer';
 
 const PageWrapper: React.FC<DefaultProps> = ({ children, className }) => {
   return (
     <div className={clsx(s.root, className)}>
       <Header />
       <main className={s.main}>{children}</main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 };
